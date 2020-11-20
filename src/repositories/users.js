413fd432d6d2ae7701ecdbe2eed01898910370d0
Lucas Evangelista <lucas.evangelista@cubos.io>
2020-11-20 17:45:17 -0300

    fix: fix on createUser and add getUser

--- src/repositories/users.js
+++ src/repositories/users.js
@@ -1,15 +1,26 @@
 const db = require('../utils/database');
 
-const createUser = async (ctx) => {
+const createUser = async (obj) => {
 	const query = {
 		text: `INSERT INTO users
-		(name,email,password)
-		VALUES($1,$2,$3) RETURNING id`,
-		values: [ctx.nome, ctx.email, ctx.senha],
+		(first_name,last_name,email,password)
+		VALUES($1,$2,$3,$4) RETURNING id`,
+		values: [obj.first_name, obj.last_name, obj.email, obj.password],
 	};
 
 	const result = await db.query(query);
 	return result.rows.shift();
 };
 
-module.exports = { createUser };
+const getUser = async (email) => {
+	const query = {
+		text: `SELECT * FROM users
+		WHERE email like $1`,
+		values: [email],
+	};
+
+	const result = await db.query(query);
+	return result.rows.shift();
+};
+
+module.exports = { createUser, getUser };
