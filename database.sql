90391fbbd312da5017d50671c9f8a9190db27d99
Lucas Evangelista <lucas.evangelista@cubos.io>
2020-11-21 01:41:09 -0300

    fix: database query

--- database.sql
+++ database.sql
@@ -1,26 +1,22 @@
 create table clients (
 	id SERIAL primary key,
 	user_id INTEGER not null,
-	FOREIGN key(user_id) references users(id),
 	cpf numeric not NULL,
 	email text,
 	tel NUMERIC,
-	first_name text not NULL,
-	last_name text not NULL
+	name text not null
 )
 
 create table users (
 	id SERIAL primary key,
 	email text not NULL,
 	password text not NULL,
-	first_name text not NULL,
-	last_name text not NULL
+	name text not NULL
 )
 
 create table charges (
 	id SERIAL primary key,
 	client_id integer not null,
-	FOREIGN key (client_id) references clients(id),
 	description text,
 	amount numeric not NULL,
 	expiration_date TIMESTAMP,
