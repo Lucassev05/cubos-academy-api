59ce87732423e97a77a827e115ca015c58d44435
Lucas Evangelista <lucas.evangelista@cubos.io>
2020-11-22 22:51:36 -0300

    feat: email validation on users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,18 +1,25 @@
 const Users = require('../repositories/users');
 const response = require('../utils/response');
+const Validator = require('../utils/validator');
 
 const createUser = async (ctx) => {
 	const { nome = null, email = null } = ctx.request.body;
 	const { hash = null } = ctx.state;
 
 	if (!nome || !email) {
-		return response(ctx, 400, { message: 'Pedido mal formatado' });
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado' });
+	}
+
+	const validEmail = Validator.email(email);
+
+	if (!validEmail) {
+		return response(ctx, 400, { mensagem: 'Email inválido' });
 	}
 
 	const existence = await Users.getUser(email);
 
 	if (existence) {
-		return response(ctx, 400, { message: 'Usuário já cadastrado' });
+		return response(ctx, 400, { mensagem: 'Usuário já cadastrado' });
 	}
 
 	const user = {
