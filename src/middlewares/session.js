d4139e8bddfa8ccfbaa09d6410b70a7cf81b2abb
Lucas Evangelista <lucas.evangelista@cubos.io>
2020-11-22 22:50:06 -0300

    feat: email validation on session

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -1,17 +1,26 @@
 const jwt = require('jsonwebtoken');
 const response = require('../utils/response');
+require('dotenv').config();
 
 const verify = async (ctx, next) => {
-	// eslint-disable-next-line no-unused-vars
-	const [bearer, token] = ctx.headers.authorization.split(' ');
+	if (!ctx.headers.authorization) {
+		return response(ctx, 401, {
+			message: 'Pedido mal formatado',
+		});
+	}
 
 	try {
+		// eslint-disable-next-line no-unused-vars
+		const [bearer, token] = ctx.headers.authorization.split(' ');
 		const verification = await jwt.verify(token, process.env.JWT_SECRET);
 
 		ctx.state.userId = verification.id;
 		ctx.state.email = verification.email;
 	} catch (error) {
-		return response(ctx, 403, { message: 'Ação proibida' });
+		console.log(error);
+		return response(ctx, 403, {
+			message: 'Ocorreu um erro, por favor, tente novamente',
+		});
 	}
 	return next();
 };
